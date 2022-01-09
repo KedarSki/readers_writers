f71a102f1be18381170204dc4e46b6d255aac7b9
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2022-01-09 01:01:17 +0100

    Potentially Resolved

--- ReadersWriters/Program.cs
+++ ReadersWriters/Program.cs
@@ -1,2 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int value1 = 10000;
+int value2 = 10000;
+
+object _lock = new object();
+
+void Writer()
+{
+    while (true)
+    {
+        lock( _lock )
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer: Enters");
+            value1++;
+            value2--;
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer:  Writing..");
+            Thread.Sleep(100);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer: Job Done!.. Leaving");
+        }
+
+    }
+}
+
+void Reader()
+{
+
+    while (true)
+    {
+        lock(_lock)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Enters");
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Reading {value1} and {value2}");
+            Thread.Sleep(200);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Job Done!.. Leaving");
+        }
+
+    }
+
+}
+
+Thread w1 = new Thread(() =>
+{
+    Writer();
+});
+
+Thread w2 = new Thread(() =>
+{
+    Writer();
+});
+
+Thread r1 = new Thread(() =>
+{
+    Reader();
+});
+
+Thread r2 = new Thread(() =>
+{
+    Reader();
+});
+
+Thread r3 = new Thread(() =>
+{
+    Reader();
+});
+
+Thread r4 = new Thread(() =>
+{
+    Reader();
+
+});
+
+Thread r5 = new Thread(() =>
+{
+    Reader();
+});
+
+w1.Start();
+w2.Start();
+
+r1.Start();
+r2.Start();
+r3.Start();
+r4.Start();
+r5.Start();
