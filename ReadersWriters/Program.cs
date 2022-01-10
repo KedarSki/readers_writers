ba94f88a57ae5c9216258737acc99474e9acc416
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2022-01-10 22:03:45 +0100

    Readers Writers Problem Solved. Created project for five dinning philosophes

--- ReadersWriters/Program.cs
+++ ReadersWriters/Program.cs
@@ -1,51 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-long value1 =(long) 10.000;
-long value2 =(long) 10.000;
+int value1 = 100;
+int value2 = 100;
+ReaderWriterLockSlim _rw = new ReaderWriterLockSlim();
 object _lock = new object();
 
 void Writer()
 {
+    
     while (true)
     {
-        lock( _lock )
+        lock (_lock)
         {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer: TRIES TO ENTER");
+            _rw.EnterWriteLock();
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer: Enters");
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer:  Writing..");
             value1++;
             value2--;
-            Thread.Sleep(100);
-            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer: Job Done!.. Leaving");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Writer:  Job Done! Leaving");
+            Thread.Sleep(200);
+            _rw.ExitWriteLock();
         }
 
     }
 }
 
+
+
 void Reader()
 {
-
+    
     while (true)
     {
-        lock(_lock)
-        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: TRIES TO ENTER");
+        _rw.EnterReadLock();
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Enters");
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Reading {value1} and {value2}");
             Thread.Sleep(100);
             Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} Reader: Job Done!.. Leaving");
-        }
+            Thread.Sleep(200);
+        _rw.ExitReadLock();
 
     }
-
 }
 
 Thread w1 = new Thread(() =>
 {
+
     Writer();
+
 });
 
 Thread w2 = new Thread(() =>
 {
+
     Writer();
+
 });
 
 Thread r1 = new Thread(() =>
@@ -82,3 +94,8 @@ r2.Start();
 r3.Start();
 r4.Start();
 r5.Start();
+
+
+
+
+
